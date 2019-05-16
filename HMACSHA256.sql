17a7276913d2b1f7e73fca2c3e614e4d824d4477
Bastien Durel <bastien.durel@data.fr>
2019-05-16 10:07:43 +0200

    allow key to be big (bigger than block size)

--- HMACSHA256.sql
+++ HMACSHA256.sql
@@ -21,9 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 DROP FUNCTION IF EXISTS HMACSHA256;
 
--- here email is the message generate a HMAC for
+-- here val is the message generate a HMAC for
 DELIMITER //
-CREATE FUNCTION HMACSHA256(secret_key VARCHAR(36), val VARCHAR(128))
+CREATE FUNCTION HMACSHA256(secret_key VARCHAR(256), val VARCHAR(2048))
   RETURNS CHAR(64) DETERMINISTIC
 BEGIN
 DECLARE ipad,opad BINARY(64);
@@ -32,7 +32,9 @@ DECLARE hmac CHAR(64);
 
 SET hexkey = RPAD(HEX(secret_key),128,"0");
 
-
+IF LENGTH(secret_key) > 64 THEN
+   SET hexkey = RPAD(SHA2(secret_key, '256'), 128, "0");
+END IF;
 
 SET ipad = UNHEX(CONCAT(
 LPAD(CONV(CONV( MID(hexkey,1  ,16), 16, 10 ) ^ CONV( '3636363636363636', 16, 10 ),10,16),16,"0"),
